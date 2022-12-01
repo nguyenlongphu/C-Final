b2741203fbd02969f1c1cc907a21ddad9d49d004
manhdong123 <91738644+manhdong123@users.noreply.github.com>
2022-12-01 15:12:16 +0700

    Add files via upload

---
+++ C-Final-main/CSharpFinal/CSharpFinal/DataLayer/Danhsachuser.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFinal.DataLayer
+{
+    internal class Danhsachuser
+    {
+        //Thao tác thông qua các thuộc tính user thông qua instance .Dùng instance để hạn chế nhiều lỗi 
+        private static Danhsachuser instance;
+        List<User> users;
+
+        public static Danhsachuser Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Danhsachuser();
+                return instance;
+
+            }
+            set => instance = value;
+        }
+
+        public List<User> Users { get => users; set => users = value; }
+
+        //Hàm khởi tạo danh sách user 
+        Danhsachuser()
+
+        {
+            users = new List<User>();
+            users.Add(new User(1, "admin", "admin", "Nguyen Minh Phuc", true));
+            users.Add(new User(2, "user01", "1234", "Nguyen Hoang Hiep", true));
+            users.Add(new User(3, "user02", "1234", "Nguyen Long Phu", false));
+            users.Add(new User(4, "user03", "12345", "Mai Truong Duy", false));
+            users.Add(new User(5, "user04", "11112", "Ho Manh Dong", true));
+            users.Add(new User(6, "user05", "123", "Nguyen Hoang Anh", true));
+
+
+        }
+
+    }
+}
---
+++ C-Final-main/CSharpFinal/CSharpFinal/DataLayer/User.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFinal.DataLayer
+{
+    internal class User : IComparable
+    {
+
+        private int iD;
+        private string taiKhoan;
+        private string matKhau;
+        private string hoVaTen;
+        private bool nhoMatKhau;
+
+        public int ID { get => iD; set => iD = value; }
+        public string TaiKhoan { get => taiKhoan; set => taiKhoan = value; }
+        public string MatKhau { get => matKhau; set => matKhau = value; }
+        public string HoVaTen { get => hoVaTen; set => hoVaTen = value; }
+        public bool NhoMatKhau { get => nhoMatKhau; set => nhoMatKhau = value; }
+
+
+        public override string ToString()
+        {
+            return string.Format("{0},{1},{2},{3},{4}", ID, TaiKhoan, MatKhau, HoVaTen, NhoMatKhau.ToString());
+        }
+
+        public int CompareTo(object obj)
+        {
+            throw new NotImplementedException();
+        }
+        public User(int iD, string taiKhoan, string matKhau, string hoVaTen, bool nhoMatKhau)
+        {
+            this.iD = 0;
+            this.taiKhoan = taiKhoan;
+            this.matKhau = matKhau;
+            this.hoVaTen = hoVaTen;
+            this.nhoMatKhau = nhoMatKhau;
+        }
+
+    }
+}
